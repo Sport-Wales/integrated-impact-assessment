bc3b6edd9258d8b7fe60532f65f4b99db27fa646
Yusra <yusrajhussain@gmail.com>
2025-05-14 09:55:31 +0100

    Added form3 step2 page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -22,6 +22,7 @@ import Form2Step1 from './pages/Form2/Step1';
 
 // Form 3 Components
 import Form3Step1 from './pages/Form3/Step1';
+import Form3Step2 from './pages/Form3/Step2';
 
 // Context Provider
 import { FormProvider } from './context/FormContext';
@@ -54,6 +55,7 @@ function App() {
               
               {/* Form 3 Routes */}
               <Route path="/form3/step1" element={<Form3Step1 />} />
+              <Route path="/form3/step2" element={<Form3Step2 />} />
             </Routes>
           </main>
         </div>
