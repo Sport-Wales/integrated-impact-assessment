bc1829fad34c6d9a4d66a0a26a919c5c25895791
Yusra <yusra.hussain@sport.wales>
2025-09-10 11:33:08 +0100

    changed order of step titles

--- app/src/pages/Form1/constants.jsx
+++ app/src/pages/Form1/constants.jsx
@@ -2,10 +2,10 @@ export const form1Steps = [
 	{ id: 'basic', title: 'Details' },
 	{ id: 'understanding', title: 'Understanding' },
 	{ id: 'people', title: 'People' },
+	{ id: 'wellbeing', title: 'Well-being' },
+	{ id: 'welsh', title: 'Welsh' },	
 	{ id: 'socioeconomic', title: 'Socio-Economic' },
 	{ id: 'environment', title: 'Environment' },
-	{ id: 'welsh', title: 'Welsh' },
-	{ id: 'wellbeing', title: 'Well-being' },
 	{ id: 'actions', title: 'Actions' },
 	{ id: 'review', title: 'Review' },
   ];
